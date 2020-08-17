c17331dae16dc8495fd1e61e9df5264db67bdde8
LaurinJ <laurin223@seznam.cz>
2020-08-17 21:27:02 +0200

    Added Django login view and accounts/login.html

--- events/config/settings.py
+++ events/config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'explorea.events'
+    'explorea.events',
+    'explorea.accounts'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [APPS_DIR/'templates'],
+        'DIRS': [os.path.join(APPS_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- events/config/urls.py
+++ events/config/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('explorea.events.urls')),
+    path('accounts/', include('explorea.accounts.urls')),
 ]
